974eeddf3d65c446789b34277880ace4117d27ba
leesy015599 <67689113+leesy015599@users.noreply.github.com>
2023-08-28 10:00:32 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.24 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+        if (s.Length % 2 == 0)
+            answer += s[s.Length / 2 - 1];
+        answer += s[s.Length / 2];
+        return answer;
+    }
+}
