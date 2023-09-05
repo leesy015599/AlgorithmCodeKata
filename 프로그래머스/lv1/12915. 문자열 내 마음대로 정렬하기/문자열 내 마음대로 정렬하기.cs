f8552662c7232c220877da5b18c7aa4f8aff8076
Soyee Lee <67689113+leesy015599@users.noreply.github.com>
2023-09-05 20:57:53 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 8.59 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cs
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cs
@@ -3,7 +3,7 @@ using System.Linq;
 public class Solution {
     public string[] solution(string[] strings, int n) {
         strings = strings.OrderBy(str => str).ToArray();
-        strings = strings.OrderBy(str => (str.Substring(n))[0]).ToArray();
+        strings = strings.OrderBy(str => str[n]).ToArray();
         return strings;
     }
 }
