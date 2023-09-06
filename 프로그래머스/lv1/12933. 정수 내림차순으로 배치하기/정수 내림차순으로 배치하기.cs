3c6c94ea7ec0b25553f6626eae8b471b8009c128
Soyee Lee <67689113+leesy015599@users.noreply.github.com>
2023-09-06 10:08:52 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 2.40 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cs
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cs
@@ -1,27 +1,12 @@
-using System.Collections.Generic;
+using System;
 
 public class Solution {
     public long solution(long n) {
-        List<int> list = new List<int>();
-        while (n != 0)
-        {
-            list.Add((int)(n % 10));
-            n /= 10;
-        }
         long answer = 0;
-        
-        for (int i = 9; i >= 0; i--)
-        {
-            foreach (int elem in list)
-            {
-                if (elem == i)
-                {
-                    answer *= 10;
-                    answer += elem;
-                }
-            }
-        }
-        
+        char[] a = n.ToString().ToCharArray();
+        Array.Sort (a);
+        Array.Reverse (a);
+        answer = long.Parse(new string(a));
         return answer;
     }
 }
