2ac0a885ca6953ea6cf58bd19b18d92f03685a95
Soyee Lee <67689113+leesy015599@users.noreply.github.com>
2023-09-08 10:12:36 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -4,9 +4,7 @@ public class Solution {
     public int solution(int[] numbers) {
         int answer = 45;
         for (int i = 0; i < numbers.Length; i++)
-        {
             answer -= numbers[i];
-        }
         return answer;
     }
 }
