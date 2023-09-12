fe05aca0a431a67edcf08baa62adcedfa8c2b700
Soyee Lee <67689113+leesy015599@users.noreply.github.com>
2023-09-12 10:00:54 +0900

    [level 1] Title: 소수 만들기, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.cs
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.cs
@@ -4,19 +4,15 @@ class Solution
 {
     public int solution(int[] nums)
     {
-        int[] three = new int[3];
         int primeCount = 0;
         
         for (int i = 0; i < nums.Length - 2; i++)
         {
-            three[0] = nums[i];
             for (int j = i + 1; j < nums.Length - 1; j++)
             {
-                three[1] = nums[j];
                 for (int k = j + 1; k < nums.Length; k++)
                 {
-                    three[2] = nums[k];
-                    if (isPrime(three))
+                    if (isPrime(nums[i] + nums[j] + nums[k]))
                         primeCount++;
                 }
             }
@@ -25,10 +21,8 @@ class Solution
         return primeCount;
     }
     
-    private bool isPrime(int[] three)
+    private bool isPrime(int num)
     {
-        int num = three[0] + three[1] + three[2];
-        
         for (int i = 2; i * i <= num; i++)
         {
             if (num % i == 0)
