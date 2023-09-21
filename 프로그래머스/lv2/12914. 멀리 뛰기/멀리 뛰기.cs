24696b1ac466bfe54ba6948a3a093ca8ddbf90d2
Soyee Lee <67689113+leesy015599@users.noreply.github.com>
2023-09-21 09:49:56 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
@@ -1,22 +1,13 @@
 public class Solution {
     long[] howMany = new long[2000];
     
-    private long HowMany(int n)
-    {
-        if (howMany[n - 1] == 0)
-        {
-            if (howMany[n - 3] == 0)
-                HowMany(n - 2);
-            if (howMany[n - 2] == 0)
-                HowMany(n - 1);
-            howMany[n - 1] = (howMany[n - 3] + howMany[n - 2]) % 1234567;
-        }
-        return howMany[n - 1];
-    }
-    
     public long solution(int n) {
         howMany[0] = 1;
         howMany[1] = 2;
-        return HowMany(n);
+        
+        for (int i = 2; i < n; i++)
+            howMany[i] = (howMany[i - 2] + howMany[i - 1]) % 1234567;
+        
+        return howMany[n - 1];
     }
 }
