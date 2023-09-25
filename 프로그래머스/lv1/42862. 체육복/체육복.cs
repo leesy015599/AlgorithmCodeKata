e629d22a3a6ef79b82bd6e00afff96dd517bd735
Soyee Lee <67689113+leesy015599@users.noreply.github.com>
2023-09-25 10:17:49 +0900

    [level 1] Title: 체육복, Time: 0.32 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.cs
+++ 프로그래머스/lv1/42862. 체육복/체육복.cs
@@ -3,32 +3,28 @@ using System;
 public class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
+        
         int[] students = new int[n];
         Array.Fill(students, 1);
         for (int i = 0; i < lost.Length; i++)
             students[lost[i] - 1] -= 1;
-        for (int j = 0; j < reserve.Length; j++)
-            students[reserve[j] - 1] += 1;
-        for (int k = 0; k < n; k++)
+        for (int i = 0; i < reserve.Length; i++)
+            students[reserve[i] - 1] += 1;
+        
+        for (int i = 0; i < n-1; i++)
         {
-            if (students[k] == 0)
+            if ((students[i] == 0 && students[i+1] == 2)
+                || (students[i] == 2 && students[i+1] == 0))
             {
-                if (k != 0 && students[k - 1] == 2)
-                {
-                    students[k - 1] -= 1;
-                    students[k] += 1;
-                    answer++;
-                }
-                else if (k != (n - 1) && students[k + 1] == 2)
-                {
-                    students[k + 1] -= 1;
-                    students[k] += 1;
-                    answer++;
-                }
+                students[i] = 1;
+                students[i+1] = 1;
             }
-            else
-                answer++;
+            if (students[i] != 0)
+                ++answer;
         }
+        if (students[n-1] != 0)
+            ++answer;
+        
         return answer;
     }
 }
