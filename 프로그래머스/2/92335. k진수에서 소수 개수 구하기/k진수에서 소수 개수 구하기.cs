0ea2788a6a58be761bb80ed76f7aa4376a31fca5
Soyee Lee <67689113+lightdelight05@users.noreply.github.com>
2023-10-11 10:27:38 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 1.31 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cs
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cs
@@ -1,38 +1,46 @@
 using System;
+using System.Text;
 
-class Solution {
+public class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        string convertNum = ConvertKbinary(n, k);
-
-        int j = 0;
-        for (int i = 0; i < convertNum.Length - 1; i = j) {
-            for (j = i + 1; j < convertNum.Length && convertNum[j] != '0'; j++) ;
-            if (IsPrime(long.Parse(convertNum.Substring(i, j - i))))
-                answer++;
-        }
-        return answer;
-    }
-
-    private static bool IsPrime(long num) {
-        if (num <= 1) return false;
-
-        for (int i = 2; i <= Math.Sqrt(num); i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
-    }
-
-    private static string ConvertKbinary(int n, int k) {
-        System.Text.StringBuilder str = new System.Text.StringBuilder();
-
-        while (n > 0) {
-            str.Append(n % k);
+        StringBuilder nkBuilder = new StringBuilder();
+        
+        while (n != 0)
+        {
+            nkBuilder.Insert(0, n % k);
             n /= k;
         }
-
-        char[] charArray = str.ToString().ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
+        
+        string nk = nkBuilder.ToString();
+        string[] strary = nk.Split('0');
+        
+        for (int i = 0; i < strary.Length; i++)
+        {
+            if (strary[i].Length == 0)
+                continue;
+            if (IsPrime(long.Parse(strary[i])))
+                ++answer;
+        }
+        
+        return answer;
+    }
+    
+    // 1번, 11번 런타임 에러 나옴.
+    // 매개변수를 int->long으로 바꾸니 런타임 에러는 해결. 1번 시간초과
+    // IsPrime 내부 for문에서 너무 오래걸려서 그러는 듯?
+    // 소수 구하는 방식을 좀 더 빠른걸로 바꾸면 해결될 것 같기도?
+    private bool IsPrime(long num)
+    {
+        if (num == 1)
+            return false;
+        if (num == 2 || num == 3)
+            return true;
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
     }
 }
