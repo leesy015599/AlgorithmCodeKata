265fc6e341bd019122436974e87c8aadeb06aae7
Soyee Lee <67689113+lightdelight05@users.noreply.github.com>
2023-10-11 21:02:23 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 1.07 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cs
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cs
@@ -26,10 +26,6 @@ public class Solution {
         return answer;
     }
     
-    // 1번, 11번 런타임 에러 나옴.
-    // 매개변수를 int->long으로 바꾸니 런타임 에러는 해결. 1번 시간초과
-    // IsPrime 내부 for문에서 너무 오래걸려서 그러는 듯?
-    // 소수 구하는 방식을 좀 더 빠른걸로 바꾸면 해결될 것 같기도?
     private bool IsPrime(long num)
     {
         if (num == 1)
