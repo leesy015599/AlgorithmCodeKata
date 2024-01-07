a60c03843076e3b71c45a7a22e3b470703e99385
Soyee Lee <67689113+lightdelight05@users.noreply.github.com>
2024-01-07 16:01:31 +0900

    [level 2] Title: 카펫, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cs
+++ 프로그래머스/2/42842. 카펫/카펫.cs
@@ -5,13 +5,11 @@ public class Solution {
         int total = brown + yellow;
         for (int width = 3; width < total; width++)
         {
-            for (int height = 3;
-                 height <= width && height <= total / width;
-                 height++)
-            {
-                if (brown == 2 * width + 2 * height - 4)
-                    return (new int[2] {width, height});
-            }
+            int height = total / width;
+            if (height > width)
+                continue;
+            if (brown == 2 * width + 2 * height - 4)
+                return (new int[2] {width, height});
         }
         return null;
     }
